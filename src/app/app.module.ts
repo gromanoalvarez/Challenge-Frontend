22ca12dbe2d429cc1b8a0360256a853d1a902920
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-04-14 18:52:34 -0300

    Transformando el proyecto en PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SharedModule } from './components/shared/shared.module';
 import { AppComponent } from './app.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { ErrorComponent } from './components/error/error.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,12 @@ import { ErrorComponent } from './components/error/error.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  urlAPI: 'https://jsonplaceholder.typicode.com/'
+
 };
